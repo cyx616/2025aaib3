aeb62a7bffb38cc566ed557be9c5f94ea69332b1
cyx616 <13750333@me.mcu.edu.tw>
2025-09-10 11:48:45 +0800

    week01

---
+++ week01-4b.cpp
@@ -0,0 +1,13 @@
+///week01-4a.cpp使用c語言寫
+#include <iostream>///使用c++語言外掛
+using namespace std;///使用c++語言的命名空間
+int main()
+{
+    int a,b;
+    cin >> a>> b;///c++語言 讀資料
+    int ans = 0;
+    for(int i=a;i<=b;i++){
+        if(i%3==0)ans += i;
+    }
+   cout << ans;///c++語言 印資料
+}
